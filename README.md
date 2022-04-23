c1da6ce4824f42d8a2cc8d4a93d67cfc61adc4c7
indigorose <indigorose049@gmail.com>
2022-04-23 14:43:54 +0100

    Initial README commit and complete gameplay

---
+++ README.md
@@ -0,0 +1,8 @@
+# Tic-Tac-Toe Game
+
+A classic two-player strategy game using python loops and arrays. 
+
+## Game Play
+
+The game of Tic-tac-Toe requires two players using a respective characcter 'X' and 'O' to create a winning three-in-a-row. 
+
--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 """ Game Rules - Classic three-in-a-row game with two players. 
     Each player is assigned the character of X or O
 """
+# Constants
 logo = """  __  .__                   __                            __                 
 _/  |_|__| ____           _/  |______    ____           _/  |_  ____   ____  
 \   __\  |/ ___\   ______ \   __\__  \ _/ ___\   ______ \   __\/  _ \_/ __ \ 
  |  | |  \  \___  /_____/  |  |  / __ \\  \___  /_____/  |  | (  <_> )  ___/ 
  |__| |__|\___  >          |__| (____  /\___  >          |__|  \____/ \___  >
               \/                     \/     \/                            \/  """
-
-
-# TODO 1 - Create the board and print the board
 base_board = [' ' for x in range(10)]
-char= ['X', 'O']
+char= [' ','X', 'O']
 
+# TODO 1 - Create and print the board
 def board(base_board):
-
     print(' ' + base_board[1] + ' | ' +
           base_board[2] + ' | ' + base_board[3])
     print('-----------')
@@ -24,65 +22,89 @@ def board(base_board):
     print(' ' + base_board[7] + ' | ' +
           base_board[8] + ' | ' + base_board[9])
 
-board(base_board)
+# TODO 2 - Winning row function
+# Board winning combinations
+def winning_row(base_board, player_char):
+    return (base_board[1] == player_char and base_board[2] == player_char and base_board[3] == player_char) or \
+        (base_board[4] == player_char and base_board[5] == player_char and base_board[6] == player_char) or \
+        (base_board[7] == player_char and base_board[8] == player_char and base_board[9] == player_char) or \
+        (base_board[1] == player_char and base_board[4] == player_char and base_board[7] == player_char) or \
+        (base_board[2] == player_char and base_board[5] == player_char and base_board[8] == player_char) or \
+        (base_board[3] == player_char and base_board[6] == player_char and base_board[9] == player_char) or \
+        (base_board[1] == player_char and base_board[5] == player_char and base_board[9] == player_char) or \
+        (base_board[3] == player_char and base_board[5]
+         == player_char and base_board[7] == player_char)
 
-# TODO 2 - Append an "X" or "O" to the board, based on location
+# TODO 3 - Player input, check occupied spaces and append an 'X' or 'O' to the board, based on location
 def player_play(player):
     play = True
     if player == 1:
-        player = char[0]
+        player = char[player]
     else:
-        player = char[1]
+        player = char[player]
     while play:
         turn = input('Where would you like to move? (1-9): ')
         try:
             turn = int(turn)
             if 0 < turn < 10:
                 if base_board[turn] == ' ':
+                    base_board[turn] = (player)
                     play = False
-                    base_board[turn] = (player, turn)
+                    return board(base_board)
                 else:
                     print('This space is already occupied. Try again!')
             else:
-                print('Please use a number from 1 -9!')
+                print('Please use a number from 1-9!')
         except:
-            print('Please type a number!')
+            print('Please use a number in the range 1-9!')
 
-player_play(3,1)
+# TODO 4 - Full board check function
+def full_board(base_board):
+    if base_board.count(' ') > 1:
+        return False
+    else:
+        print('It\'s a Tie!')
+        return True
 
-# TODO 3 - Occupied space check
-def check_space(position):
-    if base_board[position] == "X" or "O":
-        return "Player has already played here"
-
-# TODO 4 - Player input
-def player_turn():
-    pass
-
-# Board winning combinations
-def winning_row(base_board, player_char):
-    return (base_board[1] == player_char and base_board[2] == player_char and base_board[3] == player_char) or \
-    (base_board[4] == player_char and base_board[5] == player_char and base_board[6] == player_char) or \
-    (base_board[7] == player_char and base_board[8] == player_char and base_board[9] == player_char) or \
-    (base_board[1] == player_char and base_board[4] == player_char and base_board[7] == player_char) or \
-    (base_board[2] == player_char and base_board[5] == player_char and base_board[8] == player_char) or \
-    (base_board[3] == player_char and base_board[6] == player_char and base_board[9] == player_char) or \
-    (base_board[1] == player_char and base_board[5] == player_char and base_board[9] == player_char) or \
-    (base_board[3] == player_char and base_board[5] == player_char and base_board[7] == player_char)
-
+# TODO 5 - Game initialisation. 
 def game():
     print(logo)
-    print("Welcome to Tic-Tac_toe, the classic two-player game.")
-    player = input('Are you player 1 or player 2? (1-2): ')
-    player_play(player=int(player))
+    print('Welcome to Tic-Tac-Toe, the classic three-in-a-row strategy game.')
+    print(board(base_board))
+    num = 0
+    while not(full_board(base_board)):
+        if num == 2:
+            return full_board(base_board) == True
+        if num == 0:
+            num += 1
+            print(f'It\'s Player {num}\'s turn.')
+            player_play(num)
+            if not winning_row(base_board, char[num]):
+                continue
+            else:
+                print(f'Congratulations, Player {num} has won!')
+                break
+        elif num == 1:
+            num += 1
+            print(f'It\'s Player {num}\'s turn.')
+            player_play(num)
+            if not winning_row(base_board, char[num]):
+                num -= 2
+                continue
+            else:
+                print(f'Congratulations, Player {num} has won!')
+                break
+        else:
+            break
 
-# TODO 5 - Game while loop.
-game_on = True
-
-while game_on:
-    game()
-    continue_game = input("Would you like to play? (Y/N): ")
-    if continue_game.upper() is "Y":
-        player_turn()
+# TODO 6 - Game while loop.
+game()
+while True:
+    play_again = input('Would you like to play again? (Y/N): ')
+    if play_again.upper() == 'Y':
+        print('\n New Game Loading... \n')
+        base_board = [' ' for x in range(10)]
+        game()
     else:
-        game_on == False
+        break
+    
